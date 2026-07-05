b72c73b784467e421e5d0818538eed03eff7e114
Iwuchukwu Divine Ifunanya <ekeneifunanya@gmail.com>
2026-07-05 15:20:06 +0100

    Add .gitignore, integrate Tailwind CSS with Vite, and set up Vue Query plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -8,5 +10,10 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
     '@nuxt/image',
     '@nuxt/scripts'
-  ]
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
